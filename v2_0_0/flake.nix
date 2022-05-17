8a6f37ed40ee92bfd7be8c37cf254a3469b93269
hugosenari <hugosenari@gmail.com>
2022-05-17 02:25:26 -0300

    chore: re index awss3

--- v2_0_0/flake.nix
+++ v2_0_0/flake.nix
@@ -7,16 +7,16 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-awsS3-v1_0_1.flake = false;
-  inputs.src-awsS3-v1_0_1.ref   = "refs/tags/v1.0.1";
-  inputs.src-awsS3-v1_0_1.owner = "ThomasTJdev";
-  inputs.src-awsS3-v1_0_1.repo  = "nim_awsS3";
-  inputs.src-awsS3-v1_0_1.type  = "github";
+  inputs.src-awsS3-v2_0_0.flake = false;
+  inputs.src-awsS3-v2_0_0.ref   = "refs/tags/v2.0.0";
+  inputs.src-awsS3-v2_0_0.owner = "ThomasTJdev";
+  inputs.src-awsS3-v2_0_0.repo  = "nim_awsS3";
+  inputs.src-awsS3-v2_0_0.type  = "github";
   
   inputs."sigv4".owner = "nim-nix-pkgs";
   inputs."sigv4".ref   = "master";
   inputs."sigv4".repo  = "sigv4";
-  inputs."sigv4".dir   = "1_2_1";
+  inputs."sigv4".dir   = "1_3_0";
   inputs."sigv4".type  = "github";
   inputs."sigv4".inputs.nixpkgs.follows = "nixpkgs";
   inputs."sigv4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
@@ -32,13 +32,13 @@
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-awsS3-v1_0_1"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-awsS3-v2_0_0"];
     over = if builtins.pathExists ./override.nix 
            then { override = import ./override.nix; }
            else { };
   in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
-    src  = deps."src-awsS3-v1_0_1";
+    src  = deps."src-awsS3-v2_0_0";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   } );
